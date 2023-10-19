5e37bac763dc1cfc96e5fdc61f0ffeb9e85e6172
Grace Crandall <graceac9@uw.edu>
2023-10-19 10:32:20 -0700

    add code

---
+++ code/19-seastar-armcounts-code.R
@@ -0,0 +1,7 @@
+## Grace's code for working with sea star arm count data 
+
+#read in .csv
+ssarms <- read.csv("data/19-seastar-armcounts.csv", row.names = FALSE)
+
+#chart the star IDs by the arm counts
+
